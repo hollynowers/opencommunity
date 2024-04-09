a11482313bfee80af7812bd2e75041f3c6bfedbb
tshepo-ncube <m.tshepo.ncube@gmail.com>
2024-04-09 15:32:32 +0200

    register function ready for review

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,51 +1,27 @@
 import React, { useState, useEffect } from "react";
 
 function Register() {
-  const [signedIn, setSignedIn] = useState(null);
-  const [user, setUser] = useState(null);
-
   const [name, setName] = useState(null);
-  const [error, setError] = useState(null);
-  const [email, setEmail] = useState(null);
+  const [surname, setSurname] = useState(null);
+  const [username, setUsername] = useState(null);
   const [password, setPassword] = useState(null);
   const [confirmPassword, setConfirmPassword] = useState(null);
-  const [diet, setDiet] = useState();
+  const [diet, setDiet] = useState(null);
+  const [error, setError] = useState(null);
 
   useEffect(() => {}, []);
   return (
     <div>
-      <section
-        className="bg-gray-50 dark:bg-gray-900"
-        style={{ marginTop: 40 }}
-      >
-        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+      <section className="bg-gray-50 dark:bg-gray-900" style={{ marginTop: 0 }}>
+        <center>
+          <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+            Register
+          </h1>
+        </center>
+        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto  lg:py-0">
           <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-              <center>
-                <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                  Register
-                </h1>
-              </center>
               <form className="space-y-4 md:space-y-6" action="#">
-                {/* <div>
-                <label
-                  for="name"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >
-                  Your name
-                </label>
-                <input
-                  type="text"
-                  name="name"
-                  id="name"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  placeholder="name"
-                  onChange={(e) => {
-                    setName(e.target.value);
-                  }}
-                  required
-                ></input>
-              </div> */}
                 <div>
                   <label
                     for="name"
@@ -60,7 +36,7 @@ function Register() {
                     className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     placeholder="Name"
                     onChange={(e) => {
-                      setEmail(e.target.value);
+                      setName(e.target.value);
                     }}
                     required
                   ></input>
@@ -80,7 +56,7 @@ function Register() {
                     className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     placeholder="Surname"
                     onChange={(e) => {
-                      setEmail(e.target.value);
+                      setSurname(e.target.value);
                     }}
                     required
                   ></input>
@@ -100,10 +76,30 @@ function Register() {
                     className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     placeholder="Username"
                     onChange={(e) => {
-                      setEmail(e.target.value);
+                      setUsername(e.target.value);
                     }}
                     required
                   ></input>
+                </div>
+
+                <div>
+                  <label
+                    for="surname"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    Dietary Requirements
+                  </label>
+                  <textarea
+                    type="text"
+                    name="diet"
+                    id="diet"
+                    className="bg-gray-50 border h-20 resize-none border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="Dietary Requirements"
+                    onChange={(e) => {
+                      setSurname(e.target.value);
+                    }}
+                    required
+                  ></textarea>
                 </div>
 
                 <div>
@@ -117,7 +113,7 @@ function Register() {
                     type="password"
                     name="password"
                     id="password"
-                    placeholder="••••••••"
+                    placeholder="Password"
                     className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     onChange={(e) => {
                       setPassword(e.target.value);
@@ -125,6 +121,7 @@ function Register() {
                     required
                   ></input>
                 </div>
+
                 <div>
                   <label
                     for="password"
@@ -136,7 +133,7 @@ function Register() {
                     type="password"
                     name="password"
                     id="password"
-                    placeholder="••••••••"
+                    placeholder="Confirm Password"
                     className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     onChange={(e) => {
                       setConfirmPassword(e.target.value);
@@ -144,6 +141,7 @@ function Register() {
                     required
                   ></input>
                 </div>
+
                 <div className="flex items-center justify-between"></div>
                 {error ? (
                   <>
@@ -155,25 +153,10 @@ function Register() {
                 <button
                   type="submit"
                   onClick={() => {}}
-                  className="w-full text-white bg-blue-500 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                  className="w-full text-white bg-green-500 hover:bg-green-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
                 >
                   Register
                 </button>
-
-                <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                  Already have an account?{" "}
-                  <a
-                    className="font-medium text-blue-700 hover:underline dark:text-blue-700"
-                    onClick={() => {
-                      window.scrollTo({
-                        top: 0,
-                        behavior: "smooth", // Optional: Adds smooth scrolling animation
-                      });
-                    }}
-                  >
-                    Sign In
-                  </a>
-                </p>
               </form>
             </div>
           </div>
