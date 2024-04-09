7e202bd6a2f36ce587ce890e57c657b1f883a4f8
tshepo-ncube <m.tshepo.ncube@gmail.com>
2024-04-09 14:51:33 +0200

    tshepo's first commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
-import logo from './logo.svg';
-import React from 'react';
-import Sidebar from './components/Sidebar';
-import './App.css';n
-
+import logo from "./logo.svg";
+import React from "react";
+import Sidebar from "./components/Sidebar";
+import "./App.css";
 
 function App() {
-  return (
-    <div classname="App">
-    {/* <Sidebar /> */}
- 
-   </div>
-  );
+  return <div classname="App">{/* <Sidebar /> */}</div>;
 }
 
 export default App;
