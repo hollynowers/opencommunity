69c43effeda51aa3bea18a49baaf31d032f886ae
Holly Nowers <NWRHOL001@myuct.ac.za>
2024-04-09 13:45:48 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import React from 'react';
+import Sidebar from './components/Sidebar';
+import './App.css';n
+
+
+function App() {
+  return (
+    <div classname="App">
+    {/* <Sidebar /> */}
+ 
+   </div>
+  );
+}
+
+export default App;
